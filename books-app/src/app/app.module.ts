141146331650bbaed72aeeb31e2dc1af0c3b6b8c
sinnisingla <sinnisingla@outlook.com>
2022-04-22 09:55:37 +0530

    feat: create app layout

--- books-app/src/app/app.module.ts
+++ books-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layoutcomponent/header/header.component';
+import { FooterComponent } from './layoutcomponent/footer/footer.component';
+import { MenuComponent } from './layoutcomponent/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
